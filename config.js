7ba24e156a6ebd321b63229b69da362b17e3f40d
Tran Nguyen Tuan Anh <cyberdev666@gmail.com>
2022-07-19 21:08:57 +0700

    add tat ca file

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Àn nhong xê yooo!',
+    introDesc: `Trái đất có 7 tỉ ngừi
+    đố Na biết ai dẻ thưn nhấc`,
+    btnIntro: 'Nhấn vô đâyyy',
+    title: 'Dễ thưn nhấc thế giớiii ',
+    desc: 'Đố biếc luông :3 ',
+    btnYes: 'hong phải TNTA thì là ai nữaa',
+    btnNo: 'Na chứ aiii',
+    question:'Hỏi lại lần cúi nè ai dẻ hưn nhấc',
+    btnReply: 'Ấn vào đêy để tìm đáp án cuối cùng neee',
+    reply: 'chắc chắn là TNTA ùi :>',
+    mess: 'đùa thui phại là Bảo Ngọc chớ  😘😘',
+    messDesc: 'còn ai vào đây nữa hả công chúaaa.',
+    btnAccept: 'IB với ngừi kewt nhấc dũ trụ nàoooo :3 ',
+    messLink: 'https://m.me/as.cacf' 
+}
